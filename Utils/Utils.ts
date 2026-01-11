f66823fb9ad7550704bb0baa1206e91bb3574587
Alter <jf.lozano.n@gmail.com>
2026-01-10 22:34:48 -0600

    initial commit

---
+++ Utils/Utils.ts
@@ -0,0 +1,52 @@
+import type { Config } from "../types/types";
+export default class Utils {
+  valor: string;
+  settings: Config;
+  constructor(valor: string, settings: Config = {} as Config) {
+    this.valor = valor;
+    this.settings = settings;
+    console.log("constructor");
+  }
+  #encode(mensaje: string): string {
+    const words: string[] = mensaje.split(" ");
+    console.log("palabras", words);
+    const alfabeto = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
+    const resultado: string[] = [];
+    
+    return resultado.join(" ");
+  }
+  get Valor() {
+    console.log("Encode");
+    return this.#encode(this.valor).split("").length.toString();
+  }
+  set Valor(v: string) {
+    this.valor = v;
+  }
+}
---
+++ index.ts
@@ -0,0 +1,5 @@
+import Utils from "./Utils/Utils";
+
+const text = "Hola mi bro";
+const nText = new Utils(text);
+console.log(`${text}: ${nText.Valor}`);
